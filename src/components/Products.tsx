8c22d1e8fead1cf370ef0a2e38edb6bea0b927f3
Igor Santos <igorsantos9513@outlook.com>
2023-06-29 15:18:38 -0300

    feat: products animations

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -9,25 +9,27 @@ import coke4 from '@/img/cocacola4.png'
 type Props = {}
 
 export default function Products({}: Props) {
-  const [active, setActive] = useState(false)
-  const handleMouseOver = ()=> {
-    setActive(true)
+  const [activeC1, setActiveC1] = useState(false)
+  const [activeC2, setActiveC2] = useState(false)
+  const [activeC3, setActiveC3] = useState(false)
+  const [activeC4, setActiveC4] = useState(false)
 
-    
-  }
 
-  const handleMouseOut = ()=> {
-    setActive(false)
-
-  }
   return (
     <div className='z-10 absolute right-0 bottom-[-50px] w-1/2 flex flex-row items-center justify-center overflow-hidden'>
       
-        {active && <div className='absolute spin-blur-background h-[250px] left-[18%] top-[25%]'></div>}
-        <Image src={coke1} alt='coke 1' width={150} className='rotate-[-15deg]' onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}/>
-        <Image src={coke2} alt='coke 1' width={150} className='rotate-[5deg] ' />
-        <Image src={coke3} alt='coke 1' width={150} className='rotate-[15deg]'/>
-        <Image src={coke4} alt='coke 1' width={150} className='rotate-[-15deg]'/>
+        {activeC1 && <div className='absolute spin-blur-background h-[250px] left-[18%] top-[25%]'></div>}
+        <Image src={coke1} alt='coke 1' width={150} className='rotate-[-15deg] hover:animate-rotate-bounce-c1' onMouseOver={()=> setActiveC1(true)} onMouseOut={()=>setActiveC1(false)}/>
+
+        {activeC2 && <div className='absolute spin-blur-background-blue h-[250px] left-[40%]'></div>}
+        <Image src={coke2} alt='coke 1' width={150} className='rotate-[5deg] hover:animate-rotate-bounce-c2' onMouseOver={()=>setActiveC2(true)} onMouseOut={()=>setActiveC2(false)}/>
+        
+        {activeC3 && <div className='absolute spin-blur-background-green h-[250px] left-[63%]' ></div>}
+        <Image src={coke3} alt='coke 1' width={150} className='rotate-[15deg] hover:animate-rotate-bounce-c3' onMouseOver={()=> setActiveC3(true)} onMouseOut={()=> setActiveC3(false)}/>
+
+        {activeC4 && <div className='absolute spin-blur-background-purple h-[250px] left-[80%]' ></div>}
+        <Image src={coke4} alt='coke 1' width={150} className='rotate-[-15deg] hover:animate-rotate-bounce-c4' onMouseOver={()=>setActiveC4(true)} onMouseOut={()=>setActiveC4(false)}/>
+
     </div>
   )
 }
